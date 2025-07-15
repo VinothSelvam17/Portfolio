6fa7778681b73eab574f7a4352bc0c5b6cb3eaca
VINOTH S <153073991+VinothSelvam17@users.noreply.github.com>
2025-07-15 17:23:52 +0530

    Update HeroSection.jsx

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -15,6 +15,11 @@ export const HeroSection =() =>{
                 </h1>
 {/*                 <p className="text-lg md-text-lg text-muted-foreground max-2-2xl mx-atuo opacity-0 animate-fade-in-delay-3">I completed my Bachelor's degree in Computer Applications (BCA), and I’m currently pursuing my MCA—I've completed one year so far.</p> */}
 <p className="text-lg md-text-lg text-muted-foreground max-2-2xl mx-atuo opacity-0 animate-fade-in-delay-3">Full Stack Web Developer (MERN Stack) | React.js | Node.js | MongoDB | REST APIs. I build scalable, responsive web applications with clean UI and robust backend logic.</p>
+                <p className="text-lg md:text-lg text-muted-foreground max-w-2xl mx-auto animate-fade-in-delay-3">
+  Full Stack Web Developer (MERN Stack) | React.js | Node.js | MongoDB | REST APIs.
+  I build scalable, responsive web applications with clean UI and robust backend logic.
+</p>
+
                 <div className="pt-0 opacity-0 animate-fade-in-delay-4">
                     <a href="#projects" className="cosmic-button">
                         Viwe My Work
