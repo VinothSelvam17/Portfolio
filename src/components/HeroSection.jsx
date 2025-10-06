af0de4f830ac43f79de03e12cc3772d5b3aa01b7
VINOTH S <153073991+VinothSelvam17@users.noreply.github.com>
2025-10-06 16:20:58 +0530

    Update HeroSection.jsx

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -13,19 +13,12 @@ export const HeroSection =() =>{
                         {" "}
                         Selvam</span>
                 </h1>
-<p className="mt-2 text-lg text-gray-300">
-  Frontend Developer | React.js | Crafting clean, responsive, and dynamic web apps.
-</p>
-<a 
-  href="#projects"
-  className="mt-4 inline-block bg-blue-500 text-white px-6 py-2 rounded-lg hover:bg-blue-600 transition">
-  View My Work
-</a>
+
 
 {/* <p className="text-lg md-text-lg text-muted-foreground max-2-2xl mx-atuo opacity-0 animate-fade-in-delay-3">Full Stack Web Developer (MERN Stack) | React.js | Node.js | MongoDB | REST APIs. I build scalable, responsive web applications with clean UI and robust backend logic.</p> */}
                 <p className="text-lg md:text-lg text-muted-foreground max-w-4xl mx-auto animate-fade-in-delay-3">
-  Full Stack Web Developer (MERN Stack) | React.js | Node.js | MongoDB | REST APIs.
-  I build scalable, responsive web applications with clean UI and robust backend logic.
+    Frontend Developer | React.js | Crafting clean, responsive, and dynamic web apps.
+
 </p>
 
                 <div className="pt-0 opacity-0 animate-fade-in-delay-4">
