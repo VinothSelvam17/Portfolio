c8343b237cc049a26d009d036502c838d233e921
VINOTH S <153073991+VinothSelvam17@users.noreply.github.com>
2025-10-06 16:17:56 +0530

    Update HeroSection.jsx

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -6,13 +6,18 @@ export const HeroSection =() =>{
      >
         <div className="container max--w-4xl mx-auto text-center z-10">
             <div className="space-y-6">
-                <h1 className="text-4xl md-text-6xl font-bold tracking-tight">
-                    <span className="opacity-0 animate-fade-in">Hi, I'm  </span>
-                    <span className="text-primary opacity-0 animate-fade-in">Vinoth</span>
-                    <span className="text-gradient ml-2 opacity-0 animate-fade-in-delay-2">
-                        {" "}
-                        Selvam</span>
-                </h1>
+               <h1 className="text-4xl font-bold text-white">
+  Hi, I'm <span className="text-blue-400">Vinoth Selvam</span>
+</h1>
+<p className="mt-2 text-lg text-gray-300">
+  Frontend Developer | React.js | Crafting clean, responsive, and dynamic web apps.
+</p>
+<a 
+  href="#projects"
+  className="mt-4 inline-block bg-blue-500 text-white px-6 py-2 rounded-lg hover:bg-blue-600 transition">
+  View My Work
+</a>
+
 {/* <p className="text-lg md-text-lg text-muted-foreground max-2-2xl mx-atuo opacity-0 animate-fade-in-delay-3">Full Stack Web Developer (MERN Stack) | React.js | Node.js | MongoDB | REST APIs. I build scalable, responsive web applications with clean UI and robust backend logic.</p> */}
                 <p className="text-lg md:text-lg text-muted-foreground max-w-4xl mx-auto animate-fade-in-delay-3">
   Full Stack Web Developer (MERN Stack) | React.js | Node.js | MongoDB | REST APIs.
@@ -21,7 +26,7 @@ export const HeroSection =() =>{
 
                 <div className="pt-0 opacity-0 animate-fade-in-delay-4">
                     <a href="#projects" className="cosmic-button">
-                        Viwe My Work
+                        View My Work
                     </a>
                 </div>
             </div>
