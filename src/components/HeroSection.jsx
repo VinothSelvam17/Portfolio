c9e8b16030573d2a7342a2e9bb45e55becc5d18f
VINOTH S <153073991+VinothSelvam17@users.noreply.github.com>
2025-10-06 16:24:03 +0530

    Update HeroSection.jsx

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -17,8 +17,8 @@ export const HeroSection =() =>{
 
 {/* <p className="text-lg md-text-lg text-muted-foreground max-2-2xl mx-atuo opacity-0 animate-fade-in-delay-3">Full Stack Web Developer (MERN Stack) | React.js | Node.js | MongoDB | REST APIs. I build scalable, responsive web applications with clean UI and robust backend logic.</p> */}
                 <p className="text-lg md:text-lg text-muted-foreground max-w-4xl mx-auto animate-fade-in-delay-3">
-    Frontend Developer | React.js | Crafting clean, responsive, and dynamic web apps.
-
+     Frontend Developer | React.js | Building clean, responsive, and dynamic web apps.  
+  Passionate about crafting seamless user experiences with modern JavaScript and Tailwind CSS.
 </p>
 
                 <div className="pt-0 opacity-0 animate-fade-in-delay-4">
