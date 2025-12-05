0fcdb02bafced0f3720152a4ac8db0dda46e685e
VINOTH S <153073991+VinothSelvam17@users.noreply.github.com>
2025-12-05 11:46:25 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -32,7 +32,7 @@ export const Navbar = () =>{
                 </span>
             </a>
             {/* desktop */}
- <div className="hidden md:flex space-x-8">
+                <div className="hidden md:flex space-x-8">
                 {navItems.map((item,key)=> (
                     <a key={key} href={item.href} className="text-foreground/80 hover:text-primary transition-colors duration-300" >{item.name}</a>
                 ))}
